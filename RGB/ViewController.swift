154633eb53e07cd5234a2a128d3bc4ac94adf777
Feuerraderr <belov-vlad2008@yandex.ru>
2024-07-02 13:12:04 +0300

    logic

--- RGB/ViewController.swift
+++ RGB/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    private let minimumValue: Float = 0.0
+    private let maximumValue: Float = 1.0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 20
+        
+        sliderSetup()
+        
+        redSlider.value = 0.2
+        greenSlider.value = 0.5
+        blueSlider.value = 0.8
+        
+        redValueLabel.text = redSlider.value.formatted()
+        greenValueLabel.text = greenSlider.value.formatted()
+        blueValueLabel.text = blueSlider.value.formatted()
+        
     }
-
-
+    
+    
+    private func sliderSetup() {
+        redSlider.minimumValue = minimumValue
+        redSlider.maximumValue = maximumValue
+        redSlider.minimumTrackTintColor = .systemRed
+        
+        greenSlider.minimumValue = minimumValue
+        greenSlider.maximumValue = maximumValue
+        greenSlider.minimumTrackTintColor = .systemGreen
+        
+        blueSlider.minimumValue = minimumValue
+        blueSlider.maximumValue = maximumValue
+        blueSlider.minimumTrackTintColor = .systemBlue
+    }
 }
 
